a2b0e98c74f46efc2d13f49b3c6a04bc802bf629
Yulyana Kruhlikava <yuli_terra@mail.ru>
2021-01-07 19:30:51 +0300

    setup for project

--- setup.py
+++ setup.py
@@ -1,74 +1,74 @@
 import os
 from setuptools import setup, find_packages
 
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_version() -> str:
-    """Get current version.
-    :return: Curent version.
-    :rtype: str
+    """Get current version
+    Returns:
+        str: Current version
     """
-    path = os.path.join(BASE_DIR, "VERSION")
-    with open(path, "r") as version_file:
+    path = os.path.join(BASE_DIR, 'VERSION')
+    with open(path, 'r') as version_file:
         return version_file.read().strip()
 
 
+VERSION = get_version()
+
+
 def get_license_file() -> str:
-    """Get path file with license text.
-    :return: path
-    :rtype: str
+    """Get path file with license text
+    Returns:
+        str: path
     """
-    return os.path.join(BASE_DIR, "LICENSE")
+    return os.path.join(BASE_DIR, 'LICENSE')
 
 
 def get_long_description() -> str:
-    """Get README.md text.
-    :return: README.md data.
-    :rtype: str
+    """Get README.md text
+    Returns:
+        str: README.md data
     """
-    path = os.path.join(BASE_DIR, "README.md")
-    with open(path, "r") as readme_file:
+    path = os.path.join(BASE_DIR, 'README.md')
+    with open(path, 'r') as readme_file:
         return readme_file.read().strip()
 
 
 def get_requires() -> [str]:
-    """Requires packages.
-    If vesion is dev - add packages for development.
-    :return: list with packages name.
-    :rtype: [str]
+    """Requires packages
+    If version is dev - add packages name
+    Returns:
+        [str]: list with pakages name
     """
-    path = os.path.join(BASE_DIR, "requirements.txt")
-    with open(path, "r") as require_file:
+    path = os.path.join(BASE_DIR, 'requirements.txt')
+    with open(path, 'r') as require_file:
         packages = [
             package.strip()
-            for package in require_file.read().strip().split("\n")
+            for package in require_file.read().strip().split('\n')
         ]
-    return packages
-
 
 APP_PROPERTY = {
-    "name": "tnp",
-    "version": get_version(),
-    "author": "kryuly",
-    "author_email": "yuli_terra@mail.ru",
-    "url": "",
-    "packages": find_packages("src", exclude=["tests", "*test*"]),
-    "package_dir": {"": "src"},
-    "test_suite": "tests",
-    "include_package_data": True,
-    "license": get_license_file(),
-    "description": "Tut.by news parser",
-    "long_description": get_long_description(),
-    "long_description_content_type": "text/markdown",
-    "install_requires": get_requires(),
-    "python_requires": ">=3.8",
-    "zip_safe": False,
-    "entry_points": {"console_scripts": ["fxb = fxp.app"]},
-    "classifiers": [
-        "Development Status :: 5 Production/Stable"
-    ],
+    'name': 'tnp',
+    'version': VERSION,
+    'author': 'kryuly',
+    'author_email': '',
+    'url': '',
+    'packages': find_packages('src', exclude=['tests', '*test*']),
+    'package_dir': {'': 'src'},
+    'test_suite': 'tests',
+    'include_package_data': True,
+    'license': get_license_file(),
+    'descriprion': 'tut.by news parser',
+    'long_description': get_long_description(),
+    'long_description_content_type': 'text/markdown',
+    'install_requires': get_requires(),
+    'python_requires': '>=3.8',
+    'zip_safe': False,
+    # 'entry_points': {'console_scripts': ['ad_manager = adm.app.main:run]}
+    'classfiers': [
+        'Development status :: 5 Production/Stable'
+    ]
 }
 
 
